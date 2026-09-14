9d18016cc6fc25208d4ee07df5382cab3360ac30
Irina-64 <trubchik@mail.ru>
2026-09-14 15:03:21 +0300

    Реализовать просмотр фотографии в полноразмерном режиме

--- js/render-thumbnails.js
+++ js/render-thumbnails.js
@@ -1,13 +1,21 @@
+import { openBigPicture } from './big-picture.js';
+
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const picturesContainer = document.querySelector('.pictures');
 
-const renderThumbnail = ({url, description, likes, comments}) => {
+const renderThumbnail = (photo) => {
+  const {url, description, likes, comments} = photo;
   const thumbnail = pictureTemplate.cloneNode(true);
 
   thumbnail.querySelector('.picture__img').src = url;
   thumbnail.querySelector('.picture__img').alt = description;
   thumbnail.querySelector('.picture__likes').textContent = likes;
   thumbnail.querySelector('.picture__comments').textContent = comments.length;
+
+  thumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openBigPicture(photo);
+  });
 
   return thumbnail;
 };
